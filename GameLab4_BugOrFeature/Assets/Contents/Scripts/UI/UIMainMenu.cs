2c0e39df75790f2bd66339b9000b39d58938d4b2
zenoisangry <pedalinz@gmail.com>
2024-04-16 18:12:28 +0200

    Minor Update

--- GameLab4_BugOrFeature/Assets/Contents/Scripts/UI/UIMainMenu.cs
+++ GameLab4_BugOrFeature/Assets/Contents/Scripts/UI/UIMainMenu.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
 
 public class UIMainMenu : MonoBehaviour
@@ -12,21 +11,26 @@ public class UIMainMenu : MonoBehaviour
 
     public void Init()
     {
-        //playButton.onClick.AddListener(OnPlayButtonClick);
+        playButton.onClick.AddListener(OnPlayButtonClick);
         optionsButton.onClick.AddListener(OnOptionsButtonClick);
-        //exitButton.onClick.AddListener(OnExitButtonClick);
+        exitButton.onClick.AddListener(OnExitButtonClick);
     }
     public void SetActive(bool active)
     {
         gameObject.SetActive(active);
     }
+    private void OnPlayButtonClick()
+    {
+        GameStatesManager.instance.SetCurrentGameState(GameStatesManager.GameStates.Gameplay);
+    }
     private void OnOptionsButtonClick()
     {
         GameStatesManager.instance.SetCurrentGameState(GameStatesManager.GameStates.Options);
     }
-    private void OnQuitButtonClick()
+    private void OnExitButtonClick()
     {
         Application.Quit();
     }
-    //public UIManager.GameUI GetUIType() { return UIType; }
+    public UIManager.GameUI GetUIType()
+    { return UIManager.GameUI.MainMenu; }
 }
